03b71c96d605d5db4b888744913ad57af317c0e7
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2026-09-05 19:54:04 -0400

    Refactor BaseEnemy for style and pitfalls
    - Replace movement by adding to position (BAD) with movement by physical velocity (GOOD)
      → you should never add to a node's x and y values if it's physics-enabled. this will break all collisions.
      → instead, always use `velocity = ...` and `move_and_slide()` or `move_and_collide(velocity)`
    
    - Comment out and disable all code relating to enemy locating/targeting with Area2D collisions because...
      → the feature does not work
      → the feature is not part of our short-term development goals
      → the feature was not discussed and is not needed
      → the feature makes the code less readable
    
    - Revert BaseEnemy behavior to following a set target only. Without a target, it doesn't move.
      → this feature works correctly
      → this feature is in our short-term goals

--- godot/components/base_enemy/base_enemy.gd
+++ godot/components/base_enemy/base_enemy.gd
@@ -1,36 +1,60 @@
 class_name BaseEnemy
 extends CharacterBody2D
 
-@onready var curr_state : State = State.LOCATING
-@onready var detection_area : Area2D = $Area
-@export var center : Vector2
-@export var target : CharacterBody2D
-@export var speed : float = 0.1
-enum State {
-	LOCATING,
-	TARGETING
-}
+# why do we need this feature? also it doesn't work.
+#enum State {
+	#LOCATING,
+	#TARGETING
+#}
 
-func _ready() -> void:
-	if (get_tree().root.get_camera_2d()):
-		center = get_tree().root.get_camera_2d().get_screen_center_position()
-	else:
-		center = get_tree().root.get_visible_rect().size / 2
 
-func _process(delta: float) -> void:
-	if (curr_state == State.TARGETING):
-		move_towards_point(target.position, delta)
-	else:
-		move_towards_point(center, delta)
-	pass
+# why do we need this feature? why export center if it's changed onready? also it doesn't work.
+#@export var center: Vector2
+@export var target: CharacterBody2D
+@export var speed: float = 10000.0
+@export var stop_at_distance: float = 200
+
+# why do we need this feature? also it doesn't work
+#@onready var state: State = State.LOCATING
+#@onready var detection_area: Area2D = $Area
+
+
+# are you trying to have the enemy move toward the camera while the camera tracks the player????? 
+# I am very confused. This code does produce that output. 
+# What was wrong with the way the code was before? Why do we need this feature?
+#func _ready() -> void:
+	#if (get_tree().root.get_camera_2d()):
+		#center = get_tree().root.get_camera_2d().get_screen_center_position()
+	#else:
+		#center = get_tree().root.get_visible_rect().size / 2
+
+
+# why do we need this feature? also, it doesn't work.
+#func _physics_process(delta: float) -> void:
+	#if (state == State.TARGETING):
+		#move_towards_point(target.position, delta)
+	#else:
+		#move_towards_point(center, delta)
+	#pass
+
+
+func _physics_process(delta: float) -> void:
+	if not target: return
 	
-func move_towards_point(point : Variant, delta: float) -> void:
-	var dir_to_target : Vector2 = point - self.position
-	self.position += dir_to_target * delta * speed
+	var direction: Vector2 = global_position.direction_to(target.global_position)
+	var distance: float = global_position.distance_to(target.global_position)
+	
+	if distance > stop_at_distance:
+		self.velocity = speed * direction * delta
+		move_and_slide()
+	else:
+		self.velocity = Vector2.ZERO
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if (body.is_in_group("targets") && !target):
-		print('wow')
-		target = body
-		curr_state = State.TARGETING
-	pass # Replace with function body.
+
+# why do we need this feature? also, it doesn't work.
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+	#if (body.is_in_group("targets") && !target):
+		#print('wow')
+		#target = body
+		#state = State.TARGETING
+	pass
