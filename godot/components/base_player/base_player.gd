9bf163e76c4410645c71d64e9dd9ad2bbd082b83
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2026-09-05 19:36:06 -0400

    Refactor BasePlayer for style and pitfalls
    - Calculate player movement direction with a built-in one-liner that is already normalized
    
    - Move all physics-based code from `_process()` (BAD) to `_physics_process()` (GOOD)
      → these functions occur at different times. confusing them will cause inaccurate physics updates to occur
    
    - Move ability key handling from `_process()` (BAD) to `_input()` (GOOD)
      → movement is something that should be polled continuously every frame, so it goes in `_physics_process()`
      → however, abilities only trigger when the key is pressed, not held. moving them from `_physics_process()` to `_input()` has the same desired effect, but runs the check every *keypress* instead of every *frame*, which is much less expensive.
    
    - Make several changes to ability reference and usage to easily add more abilities later and dynamically loop through them all:
      - Add ability enum
      - Add dictionary that maps InputMap actions to this enum
      - Refactor ability cooldowns dictionary to use this enum
      - Map timers to this enum

--- godot/components/base_player/base_player.gd
+++ godot/components/base_player/base_player.gd
@@ -1,46 +1,76 @@
 class_name BasePlayer
 extends CharacterBody2D
 
-var ability_cooldowns : Dictionary = {
-	"ability_1" : false, 
-	"ability_2" : false
+
+enum Ability {
+	ABILITY_1,
+	ABILITY_2,
 }
+
+const ABILITY_ACTION_MAP: Dictionary[String, Ability] = {
+	"ability_1": Ability.ABILITY_1,
+	"ability_2": Ability.ABILITY_2,
+}
+
+
+@export var speed: float = 10000
+
+var ability_on_cooldown: Dictionary[Ability, bool] = {
+	Ability.ABILITY_1: false, 
+	Ability.ABILITY_2: false,
+}
+
+@onready var ability_timers: Dictionary[Ability, Timer] = {
+	Ability.ABILITY_1: $Cooldowns/Ability1Cooldown,
+	Ability.ABILITY_2: $Cooldowns/Ability2Cooldown,
+}
+
 
 func _ready() -> void:
 	add_to_group("Player")
 
 
-func _process(delta: float) -> void:
-	# retrieve direction in which to apply movement
-	var x_axis_input : float = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	var y_axis_input : float = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	
-	# construct normalized vector2 that contains what direction the player wants to move in
-	var move_dir : Vector2 = Vector2(x_axis_input,y_axis_input).normalized() * 50000.0 * delta
-	self.velocity = move_dir
-	
+func _physics_process(delta: float) -> void:
+	var move_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	self.velocity = move_dir * speed * delta
 	self.move_and_slide()
+
+
+func _input(event: InputEvent) -> void:
+	var ability_action: String = _get_pressed_ability(event)
+	if not ability_action: return
+
+	var ability: Ability = ABILITY_ACTION_MAP[ability_action]
+	if ability_on_cooldown[ability]: return
+	_trigger_cooldown(ability)
 	
-	if (Input.is_action_just_pressed("ability_1") and 
-		not ability_cooldowns["ability_1"]
-	):
-		ability_cooldowns["ability_1"] = true
-		var timer : Timer = $Cooldowns/Ability1Cooldown
-		timer.start()
-		print("pressed ability_1")
+	match (ability):
+		Ability.ABILITY_1:
+			pass # do the thing
+		Ability.ABILITY_2:
+			pass # do the thing
+
+
+## loops through all actions in ABILITY_ACTION_MAP.
+## returns the pressed action String if it exists. 
+## otherwise, returns an empty string "", which is [b]falsy[/b].
+func _get_pressed_ability(event: InputEvent) -> String:
+	for action: String in ABILITY_ACTION_MAP.keys():
+		if event.is_action_pressed(action):
+			return action
 	
-	if (Input.is_action_just_pressed("ability_2") and 
-		not ability_cooldowns["ability_2"]
-	):
-		ability_cooldowns["ability_2"] = true
-		var timer : Timer = $Cooldowns/Ability2Cooldown
-		timer.start()
-		print("pressed ability_2")
+	return ""
+
+
+## sets an ability's cooldown to true and starts its timer
+func _trigger_cooldown(ability: Ability) -> void:
+	ability_on_cooldown[ability] = true
+	ability_timers[ability].start()
 
 
 func _on_ability_1_cooldown_timeout() -> void:
-	ability_cooldowns["ability_1"] = false
+	ability_on_cooldown[Ability.ABILITY_1] = false
 
 
 func _on_ability_2_cooldown_timeout() -> void:
-	ability_cooldowns["ability_2"] = false
+	ability_on_cooldown[Ability.ABILITY_2] = false
