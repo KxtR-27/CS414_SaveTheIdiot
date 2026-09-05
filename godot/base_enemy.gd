de9171b850ddcef036e2fdb73278fc40d6127cd8
Alex Bratek <nbratek99@gmail.com>
2026-09-05 17:13:02 -0400

    Abstract locating code and add states

--- godot/base_enemy.gd
+++ godot/base_enemy.gd
@@ -1,16 +1,35 @@
 extends CharacterBody2D
 
+@onready var curr_state : State = State.LOCATING
+@onready var detection_area : Area2D = $Area2D
+@export var center : Vector2
 @export var target : CharacterBody2D
+@export var speed : float = 0.1
+enum State {
+	LOCATING,
+	TARGETING
+}
+
+func _ready() -> void:
+	if (get_tree().root.get_camera_2d()):
+		center = get_tree().root.get_camera_2d().get_screen_center_position()
+	else:
+		center = get_tree().root.get_visible_rect().size / 2
 
 func _process(delta: float) -> void:
-	if (target):
-		move_towards_target(delta)
-		pass
+	if (curr_state == State.TARGETING):
+		move_towards_point(target.position, delta)
+	else:
+		move_towards_point(center, delta)
+		
 	pass
 	
-func move_towards_target(delta: float) -> void:
-	var target_pos : Vector2 = target.position
-	var dir_to_target : Vector2 = target_pos - self.position
-	self.position += dir_to_target * delta
-	
-	
+func move_towards_point(point : Variant, delta: float) -> void:
+	var dir_to_target : Vector2 = point - self.position
+	self.position += dir_to_target * delta * speed
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if (body == CharacterBody2D):
+		target = body
+		curr_state = State.TARGETING
+	pass # Replace with function body.
