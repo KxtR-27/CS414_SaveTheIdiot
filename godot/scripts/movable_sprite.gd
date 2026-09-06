0b56ba4e0c163b3f5397f4364112c2fce96b265f
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2026-09-06 14:41:31 -0400

    Add sprite(sheet)s to characters, see asset_packs/
    To prevent redistribution of entire asset packs by exposing them in the repository as a whole, they are now held in asset_packs/ and can be dragged n dropped into the godot project.

---
+++ godot/scripts/movable_sprite.gd
@@ -0,0 +1,22 @@
+extends AnimatedSprite2D
+
+
+@onready var parent := self.get_parent() as CharacterBody2D
+
+
+# using process because I'm not affecting physics, only visuals
+func _process(_delta: float) -> void:
+	var moving := parent.velocity != Vector2.ZERO
+	
+	var walking := self.animation == "walk" and self.is_playing()
+	var idling := self.animation == "idle" and self.is_playing()
+	
+	if not moving and not idling:
+		play("idle")
+	elif moving and not walking:
+		play("walk")
+	
+	if parent.velocity.x < 0 and not flip_h:
+		flip_h = true
+	elif parent.velocity.x > 0 and flip_h:
+		flip_h = false
