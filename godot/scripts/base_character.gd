f61d2697fe4fdc559ce746f3b4672f5f94750834
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2026-09-11 12:34:22 -0400

    Add health_changed signal + take_damage()

--- godot/scripts/base_character.gd
+++ godot/scripts/base_character.gd
@@ -2,10 +2,21 @@
 class_name BaseCharacter
 extends CharacterBody2D
 
+
+signal health_changed
+
+
 @export_group("")
 @export var speed: float = 3000.0
-@export var health: float = 100.0
+@export var health: float = 100.0:
+	set(new_health):
+		health = new_health
+		health_changed.emit()
 
 @export_group("Components")
 @export var health_component: HealthComponent
 @export var damage_component: DamageComponent
+
+
+func take_damage(damage: float) -> void:
+	health -= damage
